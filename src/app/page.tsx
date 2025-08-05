9433fa5187d1988e20fa34f27cf6aeed52fd4f99
Allie Lapinski <alex.lapinski@gmail.com>
2025-08-04 20:21:43 -0400

    Ported over the about/main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,59 @@
-import Image from "next/image";
+import AboutHeroImage from '../../public/AllieLooktoSide.webp';
+import ServicesHero from '@/components/ServicesHero';
 
-export default function Home() {
+function App() {
+  //useSeo({ 
+  //  title: 'Alex Lapinski | Software & Automation Consultant',
+  //  description: 'Welcome to the portfolio of Alex Lapinski. Specializing in software development, test automation, and project mentorship with over 18 years of experience.' 
+  //});
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="bg-white pl-8 pr-5 md:pl-12 pt-8">
+      <div className="">
+        <h1 className="text-3xl font-semibold pb-8">Rainbow Fern Consulting</h1>
+        <div className="">
+          <img className="float-left mr-5 mb-5 w-[37%] md:w-[40%] md:float-right md:ml-8 rounded-lg" src={AboutHeroImage.src} />
+          <p className="mb-5 text-sm">Hi! I’m Alex, founder of Rainbow Fern consulting.</p>
+          <p className="mb-5 text-sm">I have worked in the software field for the last 18 years working up the corp ladder from intern to architect.</p>
+          <p className="mb-5 text-sm">Now, I am focusing on taking on remote projects, mentoring and helping develop amazing test automation suites.</p>
+          <p className="pb-5 text-sm">Take a look below at some recent projects, or details on what I offer as services.</p>
+          <p className="clear-left md:clear-right size-0">&nbsp;</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+      </div>
+
+      <section id="current-projects-hero" className="my-18">
+        <h2 className="text-md font-semibold mb-5">Current Projects</h2>
+        <div className="flex gap-0">
+          <div className="w-1/3">
+            <h3 className="text-sm font-bold mb-2">Portfolio Website</h3>
+            <p className="text-xs text-gray-600 pr-5">This site itself. Building out a new professional portfolio website.</p>
+          </div>
+
+          <div className="w-1/3">
+            <h3 className="text-sm font-bold mb-2">Receipt Parser</h3>
+            <p className="text-xs text-gray-600 pr-5">A series of python scripts and integration with AI models to parse out receipts for budgeting.</p>
+          </div>
+
+          <div className="w-1/3">
+            <h3 className="text-sm font-bold mb-2">Statement Summarizer</h3>
+            <p className="text-xs text-gray-600 pr-5">A set of python scripts to extract data from bank statements and summarize.</p>
+          </div>
+        </div>
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            href="/projects"
+            className="inline-block bg-gray-600 text-white text-sm mt-4 px-6 py-2 rounded-md hover:bg-gray-400 hover:text-gray-700 transition-colors"
+          >
+            See My Projects
+          </a>
+      </section>
+
+      <div className="w-auto">
+          <div className="border-gray-200 border-t p-4">&nbsp;</div>
+      </div>
+
+      <ServicesHero />
     </div>
-  );
+  )
 }
+
+export default App;
---
+++ src/components/ServicesHero.tsx
@@ -0,0 +1,60 @@
+import HeroImage from "../../public/ShadowForest.webp";
+
+const Services = () => {
+  const servicesData = [
+    {
+      title: "Software Engineering Consulting",
+      description:
+        "Helping teams with architecture, agile practices, and code reviews."
+    },
+    {
+      title: "Software Test Automation",
+      description:
+        "Building robust, maintainable automated test suites for continuous integration."
+    },
+    {
+      title: "Engineering Mentoring",
+      description:
+        "Guiding up-and-coming engineers and testers in best practices."
+    },
+  ];
+
+  return (
+    <section id="services" className="bg-white py-10">
+      <div className="container mx-auto pr-4">
+        <h2 className="text-1xl font-bold mb-6">What I Do</h2>
+        
+        <div className="max-w-[37%] float-right ml-5 mb-5 md:float-left md:mr-8 ">
+            <img className="rounded-lg" src={HeroImage.src} />
+            <p className="text-gray-400 text-[0.67rem] pt-2">This is shadow, our resident dog and a very very good boy.</p>
+        </div>
+
+        <div className="grid md:grid-cols-3 gap-2">
+          {servicesData.map((service, index) => (
+            <div
+              key={index}
+              className="pb-6 bg-white"
+            >
+              <h3 className="text-sm font-semibold mb-1">
+                {service.title}
+              </h3>
+              <p className="text-gray-600 text-xs">{service.description}</p>
+            </div>
+          ))}
+        </div>
+
+        <div className="mt-5">
+          <a
+            href="/contact"
+            className="inline-block bg-gray-600 text-white px-6 py-3 mr-9 rounded-md hover:bg-gray-400 hover:text-gray-700 transition-colors"
+          >
+            Contact Me
+          </a>
+          
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
