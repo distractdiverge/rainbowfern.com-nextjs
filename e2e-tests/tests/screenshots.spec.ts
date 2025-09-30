f2fbbaebc16700bab01ebf127e6e3ee86a2ab63a
Astrid 'Alex' Lapinski <alex.lapinski@gmail.com>
2025-09-30 14:49:15 -0400

    Replaced URL with baseURL

--- e2e-tests/tests/screenshots.spec.ts
+++ e2e-tests/tests/screenshots.spec.ts
@@ -1,20 +1,21 @@
 import { expect, test } from '@playwright/test';
 
 const SCREENSHOT_PATH = './screenshots';
+const BASE_URL = 'https://www.rainbowfern.com';
 
 test('homepage screenshot', async ({ page }) => {
-    await page.goto('https://www.rainbowfern.com', { waitUntil: 'networkidle' });
+    await page.goto(BASE_URL, { waitUntil: 'networkidle' });
     expect(await page.screenshot({ path: `${SCREENSHOT_PATH}/homepage.png`, fullPage: true })).toMatchSnapshot();
 });
 
 test('projects screenshot direct', async ({ page }) => {
-    await page.goto('https://www.rainbowfern.com/projects', { waitUntil: 'networkidle' });
+    await page.goto(`${BASE_URL}/projects`, { waitUntil: 'networkidle' });
     
     expect(await page.screenshot({ path: `${SCREENSHOT_PATH}/projects.png`, fullPage: true })).toMatchSnapshot();
 });
 
 test('projects screenshot navigated', async ({ page }) => {
-    await page.goto('https://www.rainbowfern.com', { waitUntil: 'networkidle' });
+    await page.goto(BASE_URL, { waitUntil: 'networkidle' });
     await page.getByRole('link', { name: 'Projects', exact: true }).click();
     await page.waitForURL(/\/projects/, { waitUntil: 'networkidle' });
 
@@ -22,13 +23,13 @@ test('projects screenshot navigated', async ({ page }) => {
 });
 
 test('experience screenshot', async ({ page }) => {
-    await page.goto('https://www.rainbowfern.com/experience', { waitUntil: 'networkidle' });
+    await page.goto(`${BASE_URL}/experience`, { waitUntil: 'networkidle' });
     
     expect(await page.screenshot({ path: `${SCREENSHOT_PATH}/experience.png`, fullPage: true })).toMatchSnapshot();
 });
 
 test('experience screenshot navigated', async ({ page }) => {
-    await page.goto('https://www.rainbowfern.com', { waitUntil: 'networkidle' });
+    await page.goto(BASE_URL, { waitUntil: 'networkidle' });
     await page.getByRole('link', { name: 'Experience'}).click();
     await page.waitForURL(/\/experience/, { waitUntil: 'networkidle' });
 
@@ -36,13 +37,13 @@ test('experience screenshot navigated', async ({ page }) => {
 });
 
 test('education screenshot', async ({ page }) => {
-    await page.goto('https://www.rainbowfern.com/education', { waitUntil: 'networkidle' });
+    await page.goto(`${BASE_URL}/education`, { waitUntil: 'networkidle' });
     
     expect(await page.screenshot({ path: `${SCREENSHOT_PATH}/education.png`, fullPage: true })).toMatchSnapshot();
 });
 
 test('education screenshot navigated', async ({ page }) => {
-    await page.goto('https://www.rainbowfern.com', { waitUntil: 'networkidle' });
+    await page.goto(BASE_URL, { waitUntil: 'networkidle' });
     await page.getByRole('link', { name: 'Education'}).click();
     await page.waitForURL(/\/education/, { waitUntil: 'networkidle' });
 
@@ -50,13 +51,13 @@ test('education screenshot navigated', async ({ page }) => {
 });
 
 test('contact screenshot', async ({ page }) => {
-    await page.goto('https://www.rainbowfern.com/contact', { waitUntil: 'networkidle' });
+    await page.goto(`${BASE_URL}/contact`, { waitUntil: 'networkidle' });
     
     expect(await page.screenshot({ path: `${SCREENSHOT_PATH}/contact.png`, fullPage: true })).toMatchSnapshot();
 });
 
 test('contact screenshot navigated', async ({ page }) => {
-    await page.goto('https://www.rainbowfern.com', { waitUntil: 'networkidle' });
+    await page.goto(BASE_URL, { waitUntil: 'networkidle' });
     await page.getByRole('link', { name: 'Contact', exact: true}).click();
     await page.waitForURL(/\/contact/, { waitUntil: 'networkidle' });
     await page.waitForSelector('.items-start img', { state: 'visible' });
