b8c1ceae0d48e794e8199faca6862c9ad3365c73
Astrid 'Alex' Lapinski <alex.lapinski@gmail.com>
2025-09-30 14:50:01 -0400

    removed more baseUrls

--- e2e-tests/tests/homepage.spec.ts
+++ e2e-tests/tests/homepage.spec.ts
@@ -1,19 +1,21 @@
 import { test, expect } from '@playwright/test';
 
+const BASE_URL = 'https://www.rainbowfern.com';
+
 test('has title', async ({ page }) => {
-    await page.goto('https://www.rainbowfern.com');
+    await page.goto(BASE_URL, { waitUntil: 'networkidle' });
     await expect(page).toHaveTitle('Rainbow Fern | Astrid Lapinski | Software & Automation Consultant');
 });
 
 test('projects link', async ({ page }) => {
-    await page.goto('https://www.rainbowfern.com');
+    await page.goto(BASE_URL, { waitUntil: 'networkidle' });
     await page.getByRole('link', { name: 'See My Projects' }).click();
 
     await expect(page).toHaveURL(/\/projects/);
 });
 
 test('contact me link', async ({ page }) => {
-    await page.goto('https://www.rainbowfern.com');
+    await page.goto(BASE_URL, { waitUntil: 'networkidle' });
     await page.getByRole('link', { name: 'Contact Me' }).click();
 
     await expect(page).toHaveURL(/\/contact/);
