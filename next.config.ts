00b92f5aa285545e9076de3d6b47a3c3f36d80d5
Astrid 'Alex' Lapinski <alex.lapinski@gmail.com>
2025-10-14 18:44:54 -0400

    Tweaked build to remove console.logs; added favicon; added analyzer

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import withBundleAnalyzer from '@next/bundle-analyzer';
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true'
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  compiler: {
+    removeConsole: {
+      exclude: ['error']
+    }
+  }
 };
 
 export default nextConfig;
